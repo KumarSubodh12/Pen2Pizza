eb06bc7436f43a139914cbd0134038805bd8f8d4
KumarSubodh12 <githubsubodh1212@gmail.com>
2026-04-05 23:30:11 +0530

    added full stack working project

--- server.js
+++ server.js
@@ -13,5 +13,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/pen2pizza")
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/products", require("./routes/product"));
+app.use("/api/orders", require("./routes/order"));
+app.use("/api/collab", require("./routes/collab"));
+app.use("/api/invite", require("./routes/invite"));
+app.use("/uploads", express.static("uploads"));
+app.use("/api/chat", require("./routes/chat"));
 
-app.listen(5000, ()=>console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;  // Render ke liye dynamic port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
